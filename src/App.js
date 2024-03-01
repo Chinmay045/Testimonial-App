4f488ee7242c7278f460c29dec38b99f6f0987d6
Chinmay <chinz.sandur07@gmail.com>
2024-03-01 14:25:51 +0530

    added component elements to app file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Testimonial from './components/Testimonial.jsx';
+
+const App = () => {
+  return <div>
+    <div>
+      <h1>Our Testimonials</h1>
+      <div></div>
+      <Testimonial />
+    </div>
+  </div>;
+};
+
+export default App;
