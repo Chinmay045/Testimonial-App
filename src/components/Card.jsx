a83b77c449f9096a2f3a9ea58c432117cad4c4d4
Chinmay <chinz.sandur07@gmail.com>
2024-03-01 15:09:03 +0530

    update basic details of card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
+
 
 export default function Card(props) {
     let reviews = props.reviews;
@@ -13,6 +15,20 @@ export default function Card(props) {
             <div>
                 <p>{reviews.job}</p>
             </div>
+            <div>
+                <FaQuoteLeft />
+            </div>
+            <div>
+                <p>{reviews.text}</p>
+            </div>
+            <div>
+                <FaQuoteRight />
+            </div>
+            <div>
+                <button>+</button>
+                <button>-</button>
+            </div>
+            <div>Surprise Me</div>
         </div>
     )
 }
