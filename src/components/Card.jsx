05ee3756019268fff1259b31e6d4a59015dcc76a
Chinmay <chinz.sandur07@gmail.com>
2024-03-01 17:18:03 +0530

    update buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,13 +26,7 @@ export default function Card(props) {
             <div className='text-violet-400 mx-auto mt-5'>
                 <FaQuoteRight />
             </div>
-            <div className='flex text-3xl mt-5 gap text-violet-400 font-bold  gap-3 mx-auto'>
-                <button className='hover:text-violet-500 cursor pointer'><FaChevronLeft /></button>
-                <button className='hover:text-violet-500 cursor pointer'><FaChevronRight /></button>
-            </div>
-            <div>
-                <button className='bg-violet-400 mt-1  text-white hover:bg-violet-500 transition-all duration-200 px-10 py-2 cursor-pointer rounded-md font-bold text-lg'>Surprise Me</button>
-            </div>
+
         </div>
     )
 }
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,6 +6,14 @@ export default function Testimonial(props) {
     return (
         <div>
             <Card reviews={reviews[3]} />
+
+            <div className='flex text-3xl mt-5 gap text-violet-400 font-bold  gap-3 mx-auto'>
+                <button className='hover:text-violet-500 cursor pointer'><FaChevronLeft /></button>
+                <button className='hover:text-violet-500 cursor pointer'><FaChevronRight /></button>
+            </div>
+            <div>
+                <button className='bg-violet-400 mt-1  text-white hover:bg-violet-500 transition-all duration-200 px-10 py-2 cursor-pointer rounded-md font-bold text-lg'>Surprise Me</button>
+            </div>
         </div>
     )
 }
